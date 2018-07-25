4d7eadf23e2b135df2d102f7ae97717185cc7324
Björn Heyser <bh@bjoernheyser.de>
2018-07-25 18:30:42 +0200

    version increment

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                = 'tstreslp';
-$version           = '1.0.0';
+$version           = '1.1.0';
 $ilias_min_version = '5.1.0';
 $ilias_max_version = '5.4.999';
 $responsible       = 'Björn Heyser';
